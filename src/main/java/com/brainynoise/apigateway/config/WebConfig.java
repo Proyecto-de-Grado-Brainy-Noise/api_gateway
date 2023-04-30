c35a886da6d31de9f8f432db69dfc973978720bd
Estefania Bermudez <estefania_bermudeza@javeriana.edu.co>
2023-04-30 00:46:47 -0500

    Change port

--- src/main/java/com/brainynoise/apigateway/config/WebConfig.java
+++ src/main/java/com/brainynoise/apigateway/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://frontend:4200")
+                .allowedOrigins("http://frontend:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
