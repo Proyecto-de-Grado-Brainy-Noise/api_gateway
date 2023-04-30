937c6eb0157554647bb59c32434aed3d9102177c
Estefania Bermudez <estefania_bermudeza@javeriana.edu.co>
2023-04-30 01:43:51 -0500

    Change port

--- src/main/java/com/brainynoise/apigateway/config/CorsFilter.java
+++ src/main/java/com/brainynoise/apigateway/config/CorsFilter.java
@@ -12,7 +12,7 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://frontend:4200");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:4200");
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
